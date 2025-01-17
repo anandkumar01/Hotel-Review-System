cc985e9cf9fe3271e73bffb7d4f8574750af00cd
anandkumar01 <vishwakarmaanand858@gmail.com>
2025-01-17 15:44:11 +0530

    Communication of user service and rating service to fetch all ratings of each user

--- UserService/UserService/src/main/java/com/learning/user/service/services/Impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/learning/user/service/services/Impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.learning.user.service.services.Impl;
 
+import com.learning.user.service.entities.Rating;
 import com.learning.user.service.entities.User;
 import com.learning.user.service.exceptions.ResourceNotFoundException;
 import com.learning.user.service.repositories.UserRepository;
 import com.learning.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -26,11 +36,28 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAllUser() {
-        return userRepository.findAll();
+
+        // Get all users from the database with the help of user repository
+        List<User> allUsers = userRepository.findAll();
+
+        for(User user : allUsers){
+            // Fetch rating of each user from RATING SERVICE and write to each user
+            ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), ArrayList.class);
+//            logger.info("{} ", ratingsOfUser);
+            user.setRatings(ratingsOfUser);
+        }
+        return allUsers;
     }
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User with given id is not found on server! : " + userId));
+        // Get user from database with the help of user repository
+        User user =  userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User with given id is not found on server! : " + userId));
+
+        // Fetch rating of above user from RATING SERVICE
+        ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), ArrayList.class);
+        logger.info("{} ", ratingsOfUser);
+        user.setRatings(ratingsOfUser);
+        return user;
     }
 }
